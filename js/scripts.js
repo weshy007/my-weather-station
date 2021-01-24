85d6cc4f747b6bbf0a1b4fe4295456f35b981d13
weshy007 <josephwaweru96@gmail.com>
2021-01-24 14:58:02 +0300

    the functionality is complete

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,29 @@ $(document).ready(function() {
 
         //make request to server
         $.ajax({
-            url:`http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}`,
+            url:`http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`,
         })
         .done(function(weatherdata) {
             console.log(weatherdata);
+
+            $("#profile").html(
+               `
+               <div class="container">
+               <div class="row">
+
+               <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Weather: ${weatherdata.weather[0].description}</h5>
+                    <p class="card-text">The Temperature at ${cityname} is = ${weatherdata.main.temp} &#8451; an it feels like it is ${weatherdata.main.feels_like} &#8451; </p>
+                    <a href="https://www.google.com/search?q=${cityname}" class="btn btn-primary">learn More about this place</a>
+                </div>
+                </div>
+
+                </div>
+                </div>
+                `
+            );
         });
     })
 
