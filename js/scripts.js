6ae4c990acd4be286151844a74c58f96b7045475
weshy007 <josephwaweru96@gmail.com>
2021-01-24 14:16:42 +0300

    the weather request functionality on console

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    
+    $("#searchInput").on("keyup", function(e) {
+        var cityname = e.target.value;
+        const APIKey = "972adc3384cc972b79a509ab4c6e3894";
+
+        //make request to server
+        $.ajax({
+            url:`http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}`,
+        })
+        .done(function(weatherdata) {
+            console.log(weatherdata);
+        });
+    })
+
 })
