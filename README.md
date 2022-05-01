0a1ff0dc11144b556bb70f52394302ab08e86c96
weshy <josephwaweru96@gmail.com>
2022-05-01 22:50:07 +0300

    updated animating message

---
+++ README.md
@@ -0,0 +1,25 @@
+## MY WEATHER STATION
+The system is a weather application made with JQuery and Open Weather Map API
+
+## Getting Started
+First you `git clone` the project into your machine and open it with the Virtual Studio code. 
+So that is `git clone https://github.com/weshy007/my-weather-station.git` 
+
+## TECHNOLOGIES USED
+- HTML
+- CSS 
+- JavaScript
+- jQuery
+- Bootstrap
+
+## Behavior Driven Development(BDD)
+- The user is required to enter a city name and they get the weather of the location.
+- The user gets a chance to learn more about the location from the results
+
+## Author & Contacts
+Waithaka Waweru 
+- For Feedback josephwaweru96@gmail.com
+
+## RESOURCES
+- The `typed.js` Library Documentation can be found on github https://github.com/mattboldt/typed.js/
+
---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    //typed JS
+    $("#searchInput").on("keyup", function(e) {
+        var cityname = e.target.value;
+        const APIKey = "972adc3384cc972b79a509ab4c6e3894";
+
+        //Make request to server
+        $.ajax({
+            url:`http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIKey}&units=metric`,
+        })
+        .done(function(weatherdata) {
+            console.log(weatherdata);
+
+            $("#profile").html(
+               `
+               <div class="container">
+               <div class="row">
+
+               <div class="card" style="width: 25rem; height: auto">
+                <img class="card-img-top" style="width: 25rem; height: 20rem" src="http://openweathermap.org/img/wn/${weatherdata.weather[0].icon}@2x.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Weather: ${weatherdata.weather[0].description}</h5>
+                    <p class="card-text">The Temperature in ${cityname} is  ${weatherdata.main.temp}&#8451; but it feels like it is ${weatherdata.main.feels_like}&#8451; </p>
+                    <a href="https://www.google.com/search?q=${cityname}" class="btn btn-primary">Learn More About The Place</a>
+                </div>
+                </div>
+
+                </div>
+                </div>
+                `
+            );
+        });
+    })
+
+})
